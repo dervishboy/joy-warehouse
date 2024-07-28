2085b21e8e7168891a0c1a60ad986be2de74b7bc
Ivan Andrian <ivan.andrian000@gmail.com>
2024-07-28 18:58:54 +0700

    Working on dashboard page

--- joy-wms/app/page.js
+++ joy-wms/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const router = useRouter();
 
   const handleGetStarted = () => {
-    router.push('/dashboard/Home/overview');
+    router.push('/dashboard');
   };
 
   return (
