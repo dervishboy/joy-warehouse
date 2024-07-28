d8d23bb6f8f66ca26be132243fce492340ccd171
Ivan Andrian <ivan.andrian000@gmail.com>
2024-07-28 18:55:53 +0700

    Working on dashboard page

--- joy-wms/app/(back-office)/dashboard/page.jsx
+++ joy-wms/app/(back-office)/dashboard/page.jsx
@@ -1,9 +1,15 @@
+import ActivityCard from "@/components/dashboard/ActivityCard";
+import Charts from "@/components/dashboard/Charts";
 import React from "react";
 
-export default function Page() {
+export default function Dashboard() {
     return (
-        <div>
-            <h2></h2>
+        <div className="px-4 py-4">
+            <div className="mb-4 p-2">
+                <h2 className="text-xl font-semibold">Selamat Datang!</h2>
+            </div>
+            <ActivityCard />
+            <Charts />
         </div>
     );
 }
--- joy-wms/components/dashboard/Sidebar.jsx
+++ joy-wms/components/dashboard/Sidebar.jsx
@@ -29,13 +29,13 @@ export default function Sidebar({ isOpen, toggleSidebar }) {
     return (
         <div className={`min-h-screen bg-custom-jhitam text-custom-jputih shadow-lg fixed transition-all duration-300 ${isOpen ? 'w-60' : 'w-20'}`}>
             <div className="flex flex-col">
-                <div className="bg-custom-jhitam flex space-x-2 items-center px-4 py-6">
+                <Link className="bg-custom-jhitam flex space-x-2 items-center px-4 py-6" href="/dashboard">
                     <Image src="/Logo.png" width={30} height={30} />
                     {isOpen && <span className="text-custom-jputih text-xl font-semibold">Joy WMS</span>}
-                </div>
+                </Link>
             </div>
             <nav className="flex flex-col gap-4 px-3 py-6">
-                <Link className="flex items-center space-x-2 bg-slate-950 rounded-lg p-2" href="/dashboard/Home/overview">
+                <Link className="flex items-center space-x-2 bg-slate-950 rounded-lg p-2" href="/dashboard">
                     <Home className="w-4 h-4" />
                     {isOpen && <span className="text-md font-semibold">Home</span>}
                 </Link>
