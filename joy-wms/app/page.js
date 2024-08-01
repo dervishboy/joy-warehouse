2d83fd0c8d27843a6964d225ff88951ac20655fb
Ivan Andrian <ivan.andrian000@gmail.com>
2024-08-01 22:49:36 +0700

    build a login page

--- joy-wms/app/page.js
+++ joy-wms/app/page.js
@@ -1,73 +1,105 @@
 "use client";
 
 import { useState } from 'react';
-import { Container, Box, Typography, TextField, Button, Grid, Alert, AlertTitle, AlertDescription } from '@mui/material';
+import { Container, Box, Typography, TextField, Button, Alert, AlertTitle, AlertDescription, InputAdornment, IconButton } from '@mui/material';
 import { useRouter } from 'next/navigation';
-import { AlertCircle } from '@mui/icons-material';
+import { Lock, VisibilityOff, Visibility, Person, ErrorOutline } from '@mui/icons-material';
 import Image from 'next/image';
+
 
 export default function Login() {
   const router = useRouter();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState('');
 
-  // const handleLogin = () => {
-  //   if (username === 'admin' && password === 'password') {
-  //     localStorage.setItem('role', 'admin');
-  //     router.push('/dashboard/admin');
-  //   } else if (username === 'staff' && password === 'password') {
-  //     localStorage.setItem('role', 'staff');
-  //     router.push('/dashboard/staff-gudang');
-  //   } else {
-  //     setError('Invalid username or password');
-  //   }
-  // };
+  const handleLogin = () => {
+    if (username === 'admin' && password === 'password') {
+      localStorage.setItem('role', 'admin');
+      router.push('/dashboard/admin');
+    } else if (username === 'staff' && password === 'password') {
+      localStorage.setItem('role', 'staff');
+      router.push('/dashboard/staff');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" sx={{ height: '100vh' }}>
-      <Box boxShadow={3} display="flex" borderRadius={2}>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ height: '100vh', background: 'linear-gradient(180deg, #F8B177, #FBD8BB, #FEF2E8)' }}
+    >
+      <Box boxShadow={3} display="flex" borderRadius={2} overflow="hidden">
         <Box
           display="flex"
           flexDirection="column"
           justifyContent="center"
           alignItems="center"
-          sx={{ width: 400, height: 512, bgcolor: 'green.300', borderRadius: '4px 0 0 4px' }}
+          sx={{ width: 400, height: 512, padding: 4 }}
+          className="bg-custom-jputih"
         >
           <Box width={300} textAlign="center">
-            <Typography variant="h4" component="h1" gutterBottom>
-              Selamat Datang
+            <Typography variant="h4" component="h1" gutterBottom className='font-semibold text-custom-jhitam'>
+              Login
             </Typography>
-
-            <form onSubmit={(e) => e.preventDefault()} style={{ marginTop: 16 }}>
+            <form onSubmit={(e) => e.preventDefault()} className='mt-12'>
               <TextField
                 fullWidth
                 margin="normal"
-                label="Username"
+                placeholder='Username'
                 variant="outlined"
                 onChange={(e) => setUsername(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Person fontSize='small' />
+                    </InputAdornment>
+                  )
+                }}
               />
               <TextField
                 fullWidth
                 margin="normal"
-                label="Password"
-                type="password"
+                type={showPassword ? 'text' : 'password'}
+                placeholder='Password'
                 variant="outlined"
                 onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Lock fontSize='small'/>
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleShowPassword}>
+                        {showPassword ? <Visibility fontSize='small'/> : <VisibilityOff fontSize='small'/>}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
               />
               <Button
                 fullWidth
                 variant="contained"
-                color="primary"
                 onClick={handleLogin}
-                sx={{ marginTop: 16 }}
+                sx={{ marginTop: 8, background: 'linear-gradient(180deg, #F59549, #F8B177 )' }}
+                className='hover:bg-orange-500 text-custom-jhitam font-semibold'
               >
-                Sign in
+                Login
               </Button>
 
               {error && (
                 <Alert severity="error" sx={{ marginTop: 16 }}>
-                  <AlertCircle fontSize="inherit" />
+                  <ErrorOutline fontSize="inherit" />
                   <AlertTitle>Error</AlertTitle>
                   <AlertDescription>{error}</AlertDescription>
                 </Alert>
@@ -78,15 +110,19 @@ export default function Login() {
 
         <Box
           display="flex"
+          flexDirection="column"
           justifyContent="center"
           alignItems="center"
-          sx={{ width: 400, height: 512, borderRadius: '0 4px 4px 0' }}
+          sx={{ width: 400, height: 512, background: 'linear-gradient(180deg, #F59549, #F8B177, #FBD8BB )' }}
         >
           <Image
-            src="/logo.png"
+            src="/logoo.png"
             alt="Login Banner"
-            width={30} height={30}
+            width={200} height={200}
           />
+          <Typography variant="h6" className='mt-4 font-semibold text-custom-jhitam text-center'>
+            Joy Warehouse Management System
+          </Typography>
         </Box>
       </Box>
     </Box>
